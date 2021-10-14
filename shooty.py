bc2cd41141c66590b0539041ee713f7e4492e6a4
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2021-10-14 19:32:27 +0200

    Add projectiles

--- shooty.py
+++ shooty.py
@@ -22,6 +22,7 @@ HUMAN_COLOR = Color(38, 139, 210)
 BOT_COLOR = Color(220, 50, 47)
 GUN_COLOR = Color(101, 123, 131)
 OBSTACLES_COLOR = Color(238, 232, 213)
+PROJECTILES_COLOR = Color(211, 54, 130)
 
 PLAYER_EXTENT = 0.03
 GUN_LENGTH = 15
@@ -31,6 +32,8 @@ PLAYER_ACCELERATION_DAMPENING = 0.9
 
 MAX_VELOCITY = 1e-4
 MAX_VELOCITY_SQUARED = MAX_VELOCITY * MAX_VELOCITY
+
+PROJECTILE_VELOCITY = 1e-2
 
 def to_screen_position(position):
     return position.elementwise() * SCREEN_MAX
@@ -50,6 +53,7 @@ class Player:
         self.velocity = pygame.math.Vector2(0.0, 0.0)
         self.direction = 0.0
         self.collision_position = None
+        self.gun = Gun(self)
 
     def update_direction(self, mouse_position):
         self.direction = (to_game_position(mouse_position) - self.position).as_polar()[1]
@@ -63,7 +67,11 @@ class Player:
         if self.velocity.length_squared() > MAX_VELOCITY_SQUARED:
             self.velocity.scale_to_length(MAX_VELOCITY)
 
-    def update_position(self, delta):
+    def tick(self, delta):
+        self.__update_position(delta)
+        self.gun.tick(delta)
+
+    def __update_position(self, delta):
         self.position += self.velocity * delta
 
     def collide(self):
@@ -77,23 +85,16 @@ class Player:
         self.collision_position = None
 
     def draw(self, screen):
-        screen_position = to_screen_position(self.position)
         pygame.draw.ellipse(screen, self.color, self.screen_rect(), width=1)
-        pygame.draw.line(screen, GUN_COLOR, screen_position, screen_position + self.gun_tip())
+        self.gun.draw(screen)
 
     def screen_rect(self):
         player_screen_extent = SCREEN_MAX * PLAYER_EXTENT
         left_top = to_screen_position(self.position) - (player_screen_extent / 2.0)
         return Rect(left_top.x, left_top.y, player_screen_extent.x, player_screen_extent.y)
 
-    def gun_tip(self):
-        v = pygame.math.Vector2()
-        v.from_polar((GUN_LENGTH, self.direction))
-        return v
-
     def collides_with(self, entity):
         return entity.screen_rect().colliderect(self.screen_rect())
-
 
 class Bot(Player):
     def __init__(self, position=pygame.math.Vector2(0.5, 0.5)):
@@ -150,6 +151,64 @@ class Obstacle:
             self.height * SCREEN_MAX[1]
         )
 
+class Gun:
+    def __init__(self, player):
+        self.player = player
+        self.shooting = False
+        self.cooldown_ticks = 200
+        self.ticks_elapsed = 0
+        self.projectiles = []
+
+    def tick(self, delta):
+        if self.shooting:
+            self.ticks_elapsed += delta
+
+        if self.ticks_elapsed >= self.cooldown_ticks:
+            self.shooting = False
+            self.ticks_elapsed = 0
+
+        for projectile in self.projectiles:
+            projectile.tick(delta)
+            if projectile.should_die():
+                self.projectiles.remove(projectile)
+
+    def shoot(self):
+        if self.shooting:
+            return None
+        self.shooting = True
+        self.projectiles.append(Projectile(self.player.position, self.player.direction))
+
+    def draw(self, screen):
+        screen_position = to_screen_position(self.player.position)
+        pygame.draw.line(screen, GUN_COLOR, screen_position, screen_position + self.gun_tip())
+        for projectile in self.projectiles:
+            projectile.draw(screen)
+
+    def gun_tip(self):
+        v = pygame.math.Vector2()
+        v.from_polar((GUN_LENGTH, self.player.direction))
+        return v
+
+class Projectile:
+    def __init__(self, position, direction):
+        self.position = position
+        self.direction = direction
+        v = pygame.math.Vector2()
+        v.from_polar((GUN_LENGTH, self.direction))
+        self.screen_from = to_screen_position(self.position) + v
+        self.screen_to = self.screen_from + 10 * v
+        self.ticks_to_live = 200
+        self.ticks_alive = 0
+
+    def tick(self, delta):
+        self.ticks_alive += delta
+
+    def should_die(self):
+        return self.ticks_alive > self.ticks_to_live
+
+    def draw(self, screen):
+        pygame.draw.line(screen, PROJECTILES_COLOR, self.screen_from, self.screen_to)
+
 def main():
     pygame.init() # TODO: only initialise what's necessary
 
@@ -163,6 +222,9 @@ def main():
     players = [human, random_bot(), random_bot(), random_bot(), random_bot()]
     map = Map()
     map.add_obstacle(0.1, 0.1, 0.05, 0.05)
+    map.add_obstacle(0.1, 0.7, 0.05, 0.2)
+    map.add_obstacle(0.7, 0.1, 0.2, 0.05)
+    map.add_obstacle(0.85, 0.85, 0.05, 0.05)
     delta = 0
 
     while True:
@@ -178,6 +240,10 @@ def main():
         for k, v in KEYS.items():
             if (keystate[k]):
                 human_movement_direction += v
+
+        mouse_pressed = pygame.mouse.get_pressed()
+        if mouse_pressed[0]:
+            projectile = human.gun.shoot()
 
         screen.fill(BACKGROUND_COLOR)
         map.draw(screen)
@@ -201,10 +267,7 @@ def main():
                 else:
                     player.uncollide()
 
-            player.update_position(delta)
-
-            print(player.collision_position, end='')
-
+            player.tick(delta)
             player.draw(screen)
 
         # print(' FPS: ' + str(clock.get_fps()), end='')
