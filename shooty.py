952db86d17442453a6d4d8009d959e942d737069
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2021-10-19 19:14:17 +0200

    Remove legacy code

--- shooty.py
+++ shooty.py
@@ -1,87 +1,28 @@
-import time
-import itertools
+from simulation import Simulation
+from gui import Gui
 
-import pygame
-from pygame.locals import *
 
-from constants import *
-from map import Map
-from player import Human, Bot
-from util import random_vector2
+def main(with_gui=True):
+  simulation = Simulation()
 
-def random_bot():
-    return Bot(random_vector2(min=0.1, max=0.8))
+  if with_gui:
+    gui = Gui(key_target_player=simulation.human)
+    gui.initialize()
+  else:
+    gui = None
 
-def main():
-    pygame.init() # TODO: only initialise what's necessary
+  while not simulation.over() and not (gui and gui.should_quit()):
+    simulation.tick()
 
-    winstyle = 0  # |FULLSCREEN
-    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
-    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
-    pygame.mouse.set_visible(False)
-    clock =  pygame.time.Clock()
+    if not gui is None:
+      gui.tick()
 
-    human = Human()
-    players = [human, random_bot(), random_bot(), random_bot(), random_bot()]
-    map = Map()
-    map.add_obstacle(0.1, 0.1, 0.05, 0.05)
-    map.add_obstacle(0.1, 0.7, 0.05, 0.2)
-    map.add_obstacle(0.7, 0.1, 0.2, 0.05)
-    map.add_obstacle(0.85, 0.85, 0.05, 0.05)
-    delta = 0
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT or \
-                (event.type == KEYDOWN and event.key == K_ESCAPE):
-                    return
-
-        keystate = pygame.key.get_pressed()
-        mouse_position = pygame.mouse.get_pos()
-
-        if not human.is_dead():
-            human_movement_direction = pygame.math.Vector2()
-            for k, v in KEYS.items():
-                if (keystate[k]):
-                    human_movement_direction += v
-
-            mouse_pressed = pygame.mouse.get_pressed()
-            if mouse_pressed[0]:
-                human.gun.shoot()
-
-        screen.fill(BACKGROUND_COLOR)
-        map.draw(screen)
-
-        for player in players:
-            if not player.is_dead():
-                all_projectiles = list(itertools.chain.from_iterable([p.gun.projectiles for p in set(players) - set([player])])) # flatten
-                player.update_projectile_collisions(all_projectiles)
-
-                if player == human:
-                    player.update_velocity(human_movement_direction)
-                    player.update_direction(mouse_position)
-                else:
-                    player.gun.shoot()
-                    player.randomise_direction()
-                    player.randomise_movement_direction()
-                    player.update_velocity(player.movement_direction)
-
-                if map.collides_with(player):
-                    player.collide()
-
-                for other_player in list(set(players) - set([player])):
-                    if not other_player.is_dead() and other_player.collides_with(player):
-                        player.collide()
-                    
-            player.tick(delta)
-            player.draw(screen)
-
-        # print(' FPS: ' + str(clock.get_fps()), end='')
-        print('', end='\r')
-
-        pygame.display.update()
-        delta = clock.tick()
-
+      gui.handle_key_events()
+      gui.handle_mouse_events()
+      
+      if gui.is_render_necessary():
+        gui.render(simulation)
 
 if __name__ == '__main__':
-    main()
+  # main(with_gui=False)
+  main(with_gui=True)
